10ce79a82f06d4bcc496d21cff281d812048de40
jeky22 <qhzh33@gmail.com>
2021-04-18 22:13:58 +0900

    modify skills function

--- util/data/Skillsdata.js
+++ util/data/Skillsdata.js
@@ -1,26 +1,26 @@
-export const Careerdata = {
+export const Skillsdata = {
   "Language": [
     { name: "JavaScript", percent: 80, color: "primary", imgsrc: "javascript",main:true },
     { name: "Python", percent: 60, color: "secondary", imgsrc: "python",main:false },
-    { name: "Java", percent: 40, color: "primary", imgsrc: "java",main:false },
+    { name: "Java", percent: 40, color: "secondary", imgsrc: "java",main:false },
     { name: "C++", percent: 40, color: "secondary", imgsrc: "c",main:false },
   ],
   "Front": [
     { name: "Vue.js", percent: 80, color: "primary", imgsrc: "vue",main:true },
-    { name: "React.js", percent: 60, color: "secondary", imgsrc: "react",main:true },
-    { name: "Next.js", percent: 40, color: "primary", imgsrc: "react",main:false },
-    { name: "React-Native.js", percent: 60, color: "primary", imgsrc: "react",main:false },
+    { name: "React.js", percent: 60, color: "primary", imgsrc: "react",main:true },
+    { name: "Next.js", percent: 40, color: "secondary", imgsrc: "react",main:false },
+    { name: "React-Native.js", percent: 60, color: "secondary", imgsrc: "react",main:false },
   ],
   "Back": [
     { name: "Node.js", percent: 80, color: "primary", imgsrc: "node",main:true },
     { name: "Django", percent: 60, color: "secondary", imgsrc: "django",main:false },
-    { name: "RubyOnRails", percent: 40, color: "primary", imgsrc: "rubyonrails",main:false },
+    { name: "RubyOnRails", percent: 40, color: "secondary", imgsrc: "rubyonrails",main:false },
     { name: "SpringBoot", percent: 40, color: "secondary", imgsrc: "springboot",main:false },
   ],
   "DB": [
     { name: "MySQL", percent: 50, color: "primary", imgsrc: "mysql",main:true },
     { name: "MongoDB", percent: 60, color: "secondary", imgsrc: "mongodb",main:false },
-    { name: "GraphQL", percent: 40, color: "primary", imgsrc: "graphql",main:false },
+    { name: "GraphQL", percent: 40, color: "secondary", imgsrc: "graphql",main:false },
   ],
   "Deployment": [
     { name: "AWS", percent: 80, color: "primary", imgsrc: "aws",main:true },
@@ -28,6 +28,6 @@ export const Careerdata = {
   ],
   "Library": [
     { name: "Bootstrap", percent: 80, color: "primary", imgsrc: "bootstrap",main:true },
-    { name: "Material-UI", percent: 60, color: "secondary", imgsrc: "material",main:true },
+    { name: "Material-UI", percent: 60, color: "primary", imgsrc: "material",main:true },
   ],
 }

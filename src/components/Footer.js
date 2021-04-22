b37b383f0e31f1caa2ac3bd75aeda9f4c4322c6c
jeky22 <qhzh33@gmail.com>
2021-04-23 01:51:27 +0900

    add footer br

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -62,7 +62,7 @@ export default function Footer(props) {
         <Typography variant="body1" className={classes.typo}>안녕하세요! 프론트엔드 개발자 이제찬입니다.</Typography>
         <Typography variant="body1" className={classes.typo}>대학생활에 다양한 프로젝트와 인턴십과정에서 다양한 프로젝트를 진행하였고, 이를 통해 웹 개발 업무 프로세스에 전반적인 이해력을 습득하였습니다.</Typography>
         <Typography variant="body1" className={classes.typo}>개발하면서 제가 느낀 좋은 IT 서비스는 사용자의 입장에서 쓰기편한 서비스라 생각하고, 늘 사용자의 관점을 고안하며 개발하고 있습니다. </Typography>
-        <Typography variant="body1" className={classes.typo}>현재는 거창한 업무성과보다 사용자의 작은 문제해결에 끝까지 몰입할 수 있는 신입 개발자입니다. 이런 저의 <b className={classes.typoBold}>성장가능성을 믿고 이끌어줄 회사</b>를 찾고 있습니다. </Typography>
+        <Typography variant="body1" className={classes.typo}>현재는 거창한 업무성과보다 사용자의 작은 문제해결에 끝까지 몰입할 수 있는 신입 개발자입니다.<br/> 이런 저의 <b className={classes.typoBold}>성장가능성을 믿고 이끌어줄 회사</b>를 찾고 있습니다. </Typography>
       </Box>
       <Typography variant="h5" style={{ marginTop: '30px' }} > {"<Link />"} </Typography>
 
